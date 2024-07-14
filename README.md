fc0890c67ff5f6d4fa8f848e1018182c15817b97
Kumari <ranjkumari@deloitte.com>
2024-07-14 21:01:03 +0530

    Updated

---
+++ README.md
@@ -0,0 +1,29 @@
+### Admin
+  
+- Admin can add doctors, edit doctors, delete doctors    
+- Schedule new doctors sessions, remove sessions   
+- View patients details    
+- View booking of patients    
+    
+### Doctors
+
+- View their Appointment
+- View their scheduled sessions
+- View details of patients
+- Delete account    
+- Eedit account settings
+    
+    
+### Patiens(Clients)
+  
+  - Make appointment online
+  - Create accounts themslves
+  - View their old booking
+  - Delete account
+  - Edit account settings    
+
+    
+| Admin Dashboard | Doctor Dashboard | Patient Dashboard |
+| -------| -------| -------|
+| Email: `admin@edoc.com` | Email: `doctor@edoc.com` |   Email: `patient@edoc.com` | 
+| Password: `123` |  Password: `123` |  Password: `123` |
---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/animations.css">  
+    <link rel="stylesheet" href="css/main.css">  
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
+    <title>WellnessCompass</title>
+    <style>
+        table{
+            animation: transitionIn-Y-bottom 0.5s;
+        }
+    </style>
+        
+</head>
+<body>
+    
+    <div class="full-height">
+        <center>
+        <table border="0">
+            <tr>
+                <td width="80%">
+                    <font class="edoc-logo"><img width="160px" height="50px" src="Logos/logowt.png" alt=""></font>
+                    <!-- <font class="edoc-logo-sub">| ONE STOP SOLUTION FOR OVERALL WELLBEING</font> -->
+                </td>
+                <td width="10%">
+                   <a href="login.php"  class="non-style-link"><p class="nav-item">LOGIN</p></a>
+                </td>
+                <td  width="10%">
+                    <a href="signup.php" class="non-style-link"><p class="nav-item" style="padding-right: 10px;">REGISTER</p></a>
+                </td>
+            </tr>
+            
+            <tr>
+                <td  colspan="3">
+                    <p class="heading-text">ONE STOP SOLUTION FOR OVERALL WELLBEING.</p>
+
+                </td>
+            </tr>
+            <tr>
+                <td  colspan="3">
+                    <p class="sub-text2">Take A Step Into A Better Future <br>
+                    Explore Our Wide Range of Services Curated to Benefit Each Individual</p>
+                </td>
+            </tr>
+            <tr>
+                
+                <td colspan="3">
+                    <center>
+                        <br> <br>
+                    <a href="login.php" >
+                        <input type="button" value="Start Your Journey" class="login-btn btn-primary btn" style="padding-left: 25px;padding-right: 25px;padding-top: 10px;padding-bottom: 10px;">
+                    </a>
+                </center>
+                </td>
+                
+            </tr>
+            <tr>
+                <td colspan="3">
+                   
+                </td>
+            </tr>
+        </table>
+        <p class="sub-text2 footer-hashen">A Well Being Platform.</p>
+    </center>
+    
+    </div>
+
+    <section class="bg-white border-b py-8">
+        <div class="container max-w-5xl mx-auto m-8">
+          <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            Our Services
+          </h2>
+          <div class="w-full mb-4">
+            <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          </div>
+          <div class="flex flex-wrap">
+            <div class="w-5/6 sm:w-1/2 p-6">
+              <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
+                Full Emotional Health Package
+              </h3>
+              <p class="text-gray-600 mb-8">
+                Emotional health is about how we think and feel. It is about our sense of wellbeing, our ability to cope with life events and how we acknowledge our own emotions as well as those of others. <br> <br>
+                Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make healthy choices.
+                <br> <br> Mental health is important at every stage of life, from childhood and adolescence through adulthood.
+                <br />
+                <br />
+              </p>
+            </div>
+            <div class="w-full sm:w-1/2 p-6">
+
+               <img src="https://img.freepik.com/free-vector/gradient-world-mental-health-day-background_23-2149604961.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1719705600&semt=ais_user" alt="">
+            </div>
+          </div>
+          <div class="flex flex-wrap flex-col-reverse sm:flex-row">
+            <div class="w-full sm:w-1/2 p-6 mt-6">
+
+               <img src="https://t3.ftcdn.net/jpg/02/03/32/58/360_F_203325873_kI7S14MAp2DQXGxZtLsdBPpsqPg6SSy3.jpg" alt="">
+            </div>
+            <div class="w-full sm:w-1/2 p-6 mt-6">
+              <div class="align-middle">
+                <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
+                  Full Physical Health Package
+                </h3>
+                <p class="text-gray-600 mb-8">
+                    Physical fitness is a state of health and well-being and, more specifically, the ability to perform aspects of sports, occupations, and daily activities.
+                  <br /> <br>
+                  Physical activity or exercise can improve your health and reduce the risk of developing several diseases like type 2 diabetes, cancer and cardiovascular disease. Physical activity and exercise can have immediate and long-term health benefits.
+                  <br /> <br>
+                  Most importantly, regular activity can improve your quality of life.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="bg-white border-b py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+          <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            Benefits of Using Our Platform
+          </h2>
+          <div class="w-full mb-4">
+            <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          </div>
+          <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+                <div class="w-full font-bold text-xl text-gray-800 px-6">
+                 <center>Self Help Tools</center> 
+                </div>
+                <p class="text-gray-800 text-base px-6 mb-5">
+                  <center>Wide Range of Self Help Tools like Journal, Day Planner, Mood Tracker, Community Connect and many more..</center>
+                </p>
+              </a>
+              <img width="90%" src="https://img.freepik.com/free-vector/boy-mental-health-with-flowers_603843-975.jpg?size=338&ext=jpg&ga=GA1.1.2113030492.1720051200&semt=ais_user" alt="">
+            </div>
+          </div>
+          <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+                <div class="w-full font-bold text-xl text-gray-800 px-6">
+                 <center> Physical Fitness Platform</center>
+                </div>
+                <p class="text-gray-800 text-base px-6 mb-5">
+                  <center>A whole platform designated to your fitness. Track your exercises each day. Get a summary on how you are perform and calories burnt. Workout suggestions and many more.. </center>
+                </p>
+                <img width="90%" src="https://images.ctfassets.net/o6q5esfvflvg/4Xs8MfRATi7vTaVl5RHrvz/9836552dd259cdfb2f0665226edfaa34/heartbeat-icon-on-watercolor-background-vector-id813015542.jpg" alt="">
+              </a>
+            </div>
+            <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+
+            </div>
+          </div>
+          <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+            <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+              <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+                <div class="w-full font-bold text-xl text-gray-800 px-6">
+                  <center>Doctor Connect</center>
+                </div>
+                <p class="text-gray-800 text-base px-6 mb-5">
+                  <center>Schedule Your Doctor Appointments from Our Specialists available at your own convenience. Track or Modify the Appointments using our easy to use platform</center>
+                </p>
+              </a>
+              <img src="https://www.iconpacks.net/icons/2/free-healthcare-icon-3610-thumb.png" alt="">
+            </div>
+          </div>
+        </div>
+        <center><h2>Copyright 2024 @ WellnessCompass</h2></center>
+      </section>
+
+</body>
+</html>
